da1d61e5eb59d559663ae929d94de150b77c742c
Cirleno <cirleno@yahoo.com.br>
2023-01-05 19:46:46 -0300

    feat: add package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+function start() {
+	const content = {}
+
+	content.searchTerm = askAndReturnSearchTerm()
+
+	function ashAndReturnSearchTerm() {
+		return 'TERM EXAMPLE'
+	}
+
+	console.log(content)
+}
+
+start()
